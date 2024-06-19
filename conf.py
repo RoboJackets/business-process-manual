c5ab02b9b0a4ada62d3a1a8da703ca4c629bf412
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-06-19 15:31:01 -0400

    Try to fix source_branch config

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ html_static_path = ['_static']
 html_title = "Business Process Manual"
 html_theme_options = {
     "source_repository": "https://github.com/RoboJackets/business-process-manual/",
-    "source_branch": os.environ.get("GIT_REF")[11:] if os.environ.get("GIT_REF") is not None else "main",
+    "source_branch": os.environ.get("GIT_REF", "refs/heads/main")[11:],
     "source_directory": "/",
     "footer_icons": [
         {
